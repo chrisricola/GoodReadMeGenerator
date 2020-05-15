ee9d0805aca8e8e4d3c4addde8bbace6ab51a0dd
Chris Rico <chris.r.rico@gmail.com>
2020-05-14 23:33:08 -0700

    start of commit

---
+++ Develop/index.js
@@ -0,0 +1,61 @@
+const questions = [
+    {
+        type: "input",
+        name: "name",
+        message:" Whats is your Github username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message:" Whats is your email?"
+    },
+    {
+        type: "input",
+        name: "url",
+        message:" Whats is the URL to your project?"
+    },
+    {
+        type: "input",
+        name: "project",
+        message:" Whats is the name of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message:" Please write a short description of your project"
+    },
+    {
+        type: "checkbox",
+        name: "license",
+        message:" Please write a short description of your project"
+    },
+    {
+        type: "input",
+        name: "commands",
+        message:" What commands should be run to install dependencies?"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message:" What command needs to be run for testing?"
+    },
+    {
+        type: "input",
+        name: "repo",
+        message:" What does the user nned to know about using the repo?"
+    },
+    {
+        type: "input",
+        name: "contribute",
+        message:" What does the user nned to know about contributing to the repo?"
+    },
+];
+
+function writeToFile(fileName, data) {
+}
+
+function init() {
+
+}
+
+init();
